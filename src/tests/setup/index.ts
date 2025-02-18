de5674489cac0743e55e797d792342bc98c74961
Andrey Allyson <andreyuser47@gmail.com>
2025-02-18 18:16:14 -0300

    fix: change wait for services from beforeEach to beforeAll

--- src/tests/setup/index.ts
+++ src/tests/setup/index.ts
@@ -1,10 +1,10 @@
 import axios from 'axios'
 import { env } from 'env'
 import orchestrator from 'tests/orchestrator'
-import { beforeEach } from 'vitest'
+import { beforeAll } from 'vitest'
 
 axios.defaults.baseURL = `http://localhost:${env.PORT}`
 
-beforeEach(async () => {
+beforeAll(async () => {
   await orchestrator.waitForWebserver()
 })
